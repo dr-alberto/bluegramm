eb4506bf8e900e8c89c83df30a90e3da6a16a1ea
dr-alberto <delrio.alberto@protonmail.com>
2020-09-22 19:05:24 +0200

    first commit

--- bluegramm/bluegramm.py
+++ bluegramm/bluegramm.py
@@ -1,13 +1,17 @@
 import requests
 
 class API():        
-    def info(token, url):
-        headers = {'Authorization': f'Token {token}'}
-        response = requests.post(url, headers=headers)
+    def __init__(self, token, url):
+        self.token = token
+        self.url = url
+
+    def info(self):
+        headers = {'Authorization': f'Token {self.token}'}
+        response = requests.post(self.url, headers=headers)
         return response
         
-    def prediction(token, url, data):
-        headers = {'Authorization': f'Token {token}'}
+    def prediction(self, data):
+        headers = {'Authorization': f'Token {self.token}'}
         data = {'content':data}
-        response = requests.post(url, data=data, headers=headers)
+        response = requests.post(self.url, data=data, headers=headers)
         return response
